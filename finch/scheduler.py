ca47547c22467d1f18975b45f8a0869bf85fbb7a
Tierry Hoermann <Tierry.Hoermann@meteoswiss.ch>
2022-10-07 16:56:14 +0200

    Minor improvements in scheduler.py

--- finch/scheduler.py
+++ finch/scheduler.py
@@ -6,7 +6,7 @@ import argparse
 from . import util
 from . import environment as env
 
-def start_slurm(scheduler_port: int = 8785, dashboard_port: int = 8877, cores_per_node: int = 20, memory_per_node: str = "24GiB") -> Client:
+def start_slurm(scheduler_port: int = 8785, dashboard_port: int = 8877, cores_per_node: int = 20, memory_per_node: str = "24GiB", verbose=False) -> Client:
     """
     Tries to start a new SLURM cluster scheduler at port `scheduler_port` and exposes a dashboard at port `dashboard_port`.
     A client for the scheduler is registered and returned.
@@ -17,6 +17,7 @@ def start_slurm(scheduler_port: int = 8785, dashboard_port: int = 8877, cores_pe
     ---
     - cores_per_node: int. The number of available cores per node on the cluster.
     - memory_per_node: int. The amount of available memory per node on the cluster.
+    - verbose: bool. Whether to print status information or not.
     """
     dashboard_address = f":{dashboard_port}"
     if not util.check_socket_open(port=scheduler_port):
@@ -31,14 +32,15 @@ def start_slurm(scheduler_port: int = 8785, dashboard_port: int = 8877, cores_pe
                 scheduler_options={"port": scheduler_port, "dashboard_address": dashboard_address},
                 local_directory=env.scratch_dir
             )
-        print("SLURM cluster started at address: %s" % cluster.scheduler_address)
-        print("Dashboard available at address: http://localhost%s/status" % dashboard_address)
-        print("You can scale the cluster with `cluster.scale(...)` within this console.")
+        if verbose:
+            print("SLURM cluster started at address: %s" % cluster.scheduler_address)
+            print("Dashboard available at address: http://localhost%s/status" % dashboard_address)
     else:
-        print(f"Did not start new cluster. Port {scheduler_port} is already in use.")
+        if verbose:
+            print(f"Did not start new cluster. Port {scheduler_port} is already in use.")
     return Client(f"127.0.0.1:{scheduler_port}")
 
-def start_scheduler(debug: bool = False) -> Client | None:
+def start_scheduler(debug: bool = False, verbose: bool = False) -> Client | None:
     """
     Starts a new default scheduler or connects to an existing one.
     If `debug` is `False`, a client connected to a SLURM cluster scheduler will be returned.
@@ -49,6 +51,6 @@ def start_scheduler(debug: bool = False) -> Client | None:
         dask.config.set(scheduler="synchronous")
         return None
     else:
-        client = start_slurm()
+        client = start_slurm(verbose=verbose)
         client.restart()
         return client
