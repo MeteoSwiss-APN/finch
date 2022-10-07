6bfbe1c0424dff03eeb1f38d32088e4f1f3fd810
Tierry Hoermann <Tierry.Hoermann@meteoswiss.ch>
2022-10-07 17:25:04 +0200

    Fixed small bug

--- start_scheduler.sh
+++ start_scheduler.sh
@@ -5,7 +5,7 @@
 
 srun --nodes=1 --partition=postproc --pty python -i -c "
 import finch;
-finch.start_scheduler(on_slurm=False, verbose=True);
+finch.start_scheduler(verbose=True);
 print(\"You can scale the cluster with \`cluster.scale(...)\` within this console.\");
 print(\"Exit with Ctrl-D\")
 "
