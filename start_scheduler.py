52a88e8e73ddf3209b4ace85530cccedeac2d709
Tierry Hoermann <Tierry.Hoermann@meteoswiss.ch>
2022-10-07 16:54:26 +0200

    start_scheduler script now runs on compute nodes

--- start_scheduler.py
+++
@@ -1,2 +0,0 @@
-import finch
-finch.start_scheduler()
--- start_scheduler.sh
+++ start_scheduler.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/bash -l
 
-# helper script to run a distributed scheduler from finch
+# script to run a distributed scheduler from finch on the cluster
 # running this script starts a SLURMCluster instance and opens an interactive python shell for it.
 
-python -i start_scheduler.py
+srun --nodes=1 --partition=postproc --pty python -i -c "
+import finch;
+finch.start_scheduler(on_slurm=False, verbose=True);
+print(\"You can scale the cluster with \`cluster.scale(...)\` within this console.\");
+print(\"Exit with Ctrl-D\")
+"
