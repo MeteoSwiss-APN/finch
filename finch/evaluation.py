88d6a7c43a48109e703818c0a779e8db668f0284
Tierry Hoermann <Tierry.Hoermann@meteoswiss.ch>
2022-10-14 16:56:10 +0200

    Implemented plot generation

--- finch/evaluation.py
+++ finch/evaluation.py
@@ -1,11 +1,14 @@
 from ensurepip import version
+import pathlib
 from typing import Any
 from collections.abc import Callable
 from . import Input
 from . import util
+from . import config
 import xarray as xr
 import numpy as np
 import numbers
+import pandas as pd
 
 
 def print_version_results(results: list[Any], versions: list[Input.Version]):
@@ -31,7 +34,7 @@ def create_result_array(results: list[list[float]], imps: list[Callable], versio
     The dimensions or given by the attributes of the Version class plus one dimension for the different implementations.
     The coordinates are labels for the version attributes / implementations.
     This result array is can then be used as an input for different evaluation functions.
-    The result array will contain zeros for every combination of version attributes, which is not listed in `versions`.
+    The result array will contain NaN for every combination of version attributes, which is not listed in `versions`.
     """
     if experiment_name is None:
         experiment_name = util.random_entity_name()
@@ -55,7 +58,7 @@ def create_result_array(results: list[list[float]], imps: list[Callable], versio
 
     # initialize data
     dim_sizes = [len(coords[a]) for a in coords]
-    data = np.zeros(dim_sizes, dtype=float)
+    data = np.full(dim_sizes, np.nan, dtype=float)
 
     # create array
     array = xr.DataArray(data, coords, name=experiment_name)
@@ -63,4 +66,39 @@ def create_result_array(results: list[list[float]], imps: list[Callable], versio
         for r, v in zip(result, versions):
             array.loc[{a : v.__dict__[a] for a in version_attr} | {"imp":i}] = r
     return array
-    
+
+def create_plots(results: xr.DataArray, reduction: Callable = np.nanmean):
+    """
+    Creates a series of plots for the results array.
+    The plot creation works as follows.
+    Every plot has multiple different lines, which correspond to the different implementations.
+    The y-axis indicates the value of the result, while the x-axis is a dimension of the result array.
+    For every dimension of size greater than 1, except for the 'imp' dimension, a new plot will be created.
+    The other dimensions will then be reduced by flattening and then reducing according to the given reduction function.
+
+    If the coordinates of a dimension have type `str`, a bar plot will be generated. Otherwise a standard line plot will be saved.
+    The plots will be stored in config's `plot_dir` in a directory according to the experiment name.
+    """
+
+    path = pathlib.Path(config["global"]["plot_dir"], results.name)
+    path.mkdir(parents=True, exist_ok=True)
+
+    for d in results.dims:
+        if d != "imp" and results.sizes[d] > 1:
+            to_reduce = [dd for dd in results.dims if dd != "imp" and dd != d]
+            to_plot = results.reduce(reduction, to_reduce)
+            df = pd.DataFrame({
+                i: to_plot[i] for i in results.coords["imp"]
+            })
+            if isinstance(to_plot.coords[d][0], str):
+                plot = df.plot(
+                    kind="bar", 
+                    stacked=True,
+                    xticks=to_plot.coords[d]
+                )
+            else:
+                plot = df.plot(
+                    kind = "line",
+                    xticks = to_plot.coords[d]
+                )
+            plot.get_figure().savefig(path.joinpath(d + ".svg"), format="svg")
