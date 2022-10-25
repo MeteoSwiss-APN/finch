479b81d72bf87a534d4e7e2460cbb2410948c81f
Tierry Hoermann <Tierry.Hoermann@meteoswiss.ch>
2022-10-25 17:04:49 +0200

    Implemented parallelized versions

--- zebra/src/cebra/brn.cpp
+++ zebra/src/cebra/brn.cpp
@@ -4,6 +4,7 @@
 #include <vectorclass.h>
 #include <vectormath_exp.h>
 #include <util.h>
+#include <omp.h>
 
 #define PC_G 9.80665
 #define PC_R_D 287.05
@@ -119,8 +120,72 @@ void brn_vec(
     free(tv_sum);
 }
 
+void thetav_vec_par(const double *p, const double *t, const double *qv, double *out, int m, int n, int o) {
+
+    const double pc_rvd = PC_R_V / PC_R_D;
+    const double pc_rdocp = PC_R_D / PC_CP_D;
+    const double pc_rvd_o = pc_rvd - 1.0;
+
+    #pragma omp for
+    for(int i = 0; i < m*n*o; i+=8) {
+        Vec8d ti, pi, qvi;
+        ti.load(&t[i]);
+        pi.load(&p[i]);
+        qvi.load(&qv[i]);
+        Vec8d outi = pow(P0 / pi,pc_rdocp) * ti * (1 + (pc_rvd_o * qvi / (1-qvi)));
+        outi.store(&out[i]);
+    }
+}
+
+void brn_vec_par(
+    const double *p, 
+    const double *t, 
+    const double *qv, 
+    const double *u, 
+    const double *v, 
+    const double *hhl, 
+    const double *hsurf, 
+    double *out, 
+    int m, int n, int o) {
+    
+    thetav(p, t, qv, out, m, n, o);
+
+    double *tv_sum = malloc_d(o);
+
+    Vec8d nat(0, 1, 2, 3, 4, 5, 6, 7);
+
+    // iterate over x,y locations
+    #pragma omp for
+    for(int hi = 0; hi < m*n; hi++) {
+        int i = hi*8;
+        // compute thetav cumsum for current x,y location
+        double prev = 0;
+        for(int j = 0; j < o; j++) {
+            double csj = out[i+o-1-j] + prev;
+            tv_sum[j] = csj;
+            prev = csj;
+        }
+        // compute brn
+        double last_tv = out[i+o-1];
+        double hsurfhi = hsurf[hi];
+        for(int j = 0; j < o; j+=8) {
+            int ij = i+j;
+            Vec8d oij, hhlij, tvj, uij, vij;
+            oij.load(&out[ij]);
+            hhlij.load(&hhl[ij]);
+            tvj.load(&tv_sum[j]);
+            uij.load(&u[ij]);
+            vij.load(&v[ij]);
+            Vec8d outij = PC_G * (hhlij - hsurfhi) * (oij - last_tv) * (o-j-nat) / 
+                (tvj*(uij*uij + vij*vij));
+            outij.store(&out[ij]);
+        }
+    }
+    free(tv_sum);
+}
+
 void thetav(const double *p, const double *t, const double *qv, double *out, int m, int n, int o) {
-    thetav_vec(p, t, qv, out, m, n, o);
+    thetav_vec_par(p, t, qv, out, m, n, o);
 }
 
 void brn(
@@ -133,5 +198,5 @@ void brn(
     const double *hsurf, 
     double *out, 
     int m, int n, int o) {
-    brn_vec(p, t, qv, u, v, hhl, hsurf, out, m, n, o);
+    brn_vec_par(p, t, qv, u, v, hhl, hsurf, out, m, n, o);
 }
