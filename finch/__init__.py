0449d54887d6693c195083417554d0972ec1eaf4
Tierry Hoermann <Tierry.Hoermann@meteoswiss.ch>
2022-10-26 21:52:20 +0200

    Adjusted package __init__

--- finch/__init__.py
+++ finch/__init__.py
@@ -1,15 +1,15 @@
+from . import environment as env
 from .config import config
 import os
 os.environ["GRIB_DEFINITION_PATH"] = config["data"]["grib_definition_path"]
 
-from .data import Input
-from .scheduler import client, start_scheduler, start_slurm
-from . import environment as env
-from .experiments import measure_runtimes, measure_operator_runtimes, measure_loading_times
-from . import evaluation as eval
-from .evaluation import print_version_results, print_results
-from . import data
-from . import constants as const
 from . import util
+from . import constants as const
+from .data import Input
+from . import data
+from .scheduler import start_scheduler, start_slurm_cluster
+from .experiments import measure_runtimes, measure_operator_runtimes, measure_loading_times
+from .evaluation import print_version_results, print_results
+from . import evaluation as eval
 
 from . import brn
