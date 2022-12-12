9f81d6fe6a1ad29c6e17c8298e7907f796052f55
Tierry Hörmann <Tierry.Hoermann@meteoswiss.ch>
2022-12-12 17:48:45 +0100

    Changed to full install in conda build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,2 +1,6 @@
-# install finch without dependencies
-$PYTHON setup.py develop --no-deps
+# install finch
+
+# pip install . --no-deps
+
+# Currently we must call setup.py directly, because of https://github.com/scikit-build/scikit-build/issues/768
+$PYTHON setup.py install
