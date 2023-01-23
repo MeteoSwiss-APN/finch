e44f0f3c916722892b5c2cb1c27317d57b38584a
Tierry Hörmann <Tierry.Hoermann@meteoswiss.ch>
2023-01-23 18:15:52 +0100

    Implementation of xarray version for destagger

--- finch/destagger/impl.py
+++ finch/destagger/impl.py
@@ -1,22 +1,10 @@
 """algorithm for destaggering a field."""
 
+import xarray as xr
 
-def destagger(field, dim):
-    """Destagger a field."""
-    if dim == "x" or dim == "y":
-        field_ = field
-        field_[{dim: slice(1, None)}] = (
-            field[{dim: slice(0, -1)}] + field[{dim: slice(1, None)}]
-        ) * 0.5
-        return field_
-    elif dim == "generalVerticalLayer":
-        hhl_k0 = field[{dim: slice(0, -1)}].assign_coords(
-            {dim: field[{dim: slice(0, -1)}].generalVerticalLayer}
-        )
 
-        hhl_k1 = field[{dim: slice(1, None)}].assign_coords(
-            {dim: field[{dim: slice(0, -1)}].generalVerticalLayer}
-        )
-        return (hhl_k0 + hhl_k1) * 0.5
-
-    raise RuntimeError("Unknown dimension", dim)
+def destagger_xr(dataset: xr.Dataset, dim: str = "x") -> xr.Dataset:
+    """Xarray implementation of the destagger operator."""
+    left = dataset[{dim: slice(0, -1)}]
+    right = dataset[{dim: slice(1, None)}]
+    return (left + right) * 0.5
