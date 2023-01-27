898fa8d01bdc147f877fa06d9ad505416288dd04
Tierry Hörmann <Tierry.Hoermann@meteoswiss.ch>
2023-01-27 18:58:06 +0100

    Cleaned __init__.py

--- finch/__init__.py
+++ finch/__init__.py
@@ -11,21 +11,47 @@ import os
 from .config import config as cfg
 from .config import debug, log_level, logging_format, set_debug_mode, set_log_level
 
-os.environ["GRIB_DEFINITION_PATH"] = config["data"]["grib_definition_path"]
+os.environ["GRIB_DEFINITION_PATH"] = cfg["data"]["grib_definition_path"]
 
-from . import brn
-from . import constants as const
-from . import data
-from . import evaluation as eval
-from . import scheduler, util
-from .experiments import (
-    DaskRunConfig,
-    DefaultOperator,
-    RunConfig,
-    measure_loading_times,
-    measure_operator_runtimes,
-    measure_runtimes,
-    xr_impl_runner,
-    xr_input_prep,
-    xr_run_prep,
-)
+if 1:  # this is a hack to avoid flake8 to complain about E402
+    from . import constants as const
+    from . import data
+    from . import evaluation as eval
+    from . import operators as ops
+    from . import scheduler, util
+    from .experiments import (
+        DaskRunConfig,
+        DefaultOperator,
+        RunConfig,
+        measure_loading_times,
+        measure_operator_runtimes,
+        measure_runtimes,
+        xr_impl_runner,
+        xr_input_prep,
+        xr_run_prep,
+    )
+
+__all__ = [
+    "_util",
+    "cfg",
+    "debug",
+    "log_level",
+    "logging_format",
+    "set_debug_mode",
+    "set_log_level",
+    "const",
+    "data",
+    "eval",
+    "ops",
+    "scheduler",
+    "util",
+    "DaskRunConfig",
+    "DefaultOperator",
+    "RunConfig",
+    "measure_loading_times",
+    "measure_operator_runtimes",
+    "measure_runtimes",
+    "xr_impl_runner",
+    "xr_input_prep",
+    "xr_run_prep",
+]
