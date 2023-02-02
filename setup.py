21bf4de088d599cb7ac663cd734e19c78d778463
Tierry Hörmann <Tierry.Hoermann@meteoswiss.ch>
2023-02-02 14:33:41 +0100

    Formatting of setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from skbuild import setup
 
+
 def exclude_static_libraries(cmake_manifest: list[str]):
     return list(filter(lambda name: not (name.endswith(".a")), cmake_manifest))
 
+
 setup(
-    packages = [
-        "finch",
-        "finch.brn"
-    ],
+    packages=["finch", "finch.brn"],
     include_package_data=True,
-    scripts = [
-        "scripts/finch"
-    ],
-    cmake_source_dir = "zebra",
-    cmake_process_manifest_hook = exclude_static_libraries,
+    scripts=["scripts/finch"],
+    cmake_source_dir="zebra",
+    cmake_process_manifest_hook=exclude_static_libraries,
     cmake_minimum_required_version="3.14",
-    cmake_args=["-DINSTALL_GTEST=OFF"]
+    cmake_args=["-DINSTALL_GTEST=OFF"],
 )
