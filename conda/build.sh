48f9259302edaccb1e18058790c0b9c3550bcac3
Tierry Hörmann <Tierry.Hoermann@meteoswiss.ch>
2023-02-08 21:30:32 +0100

    Adjusted conda build recipe

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,3 @@
 # install finch
 
-# pip install . --no-deps
-
-# Currently we must call setup.py directly, because of https://github.com/scikit-build/scikit-build/issues/768
-$PYTHON setup.py install
+pip install . -e --no-depss
