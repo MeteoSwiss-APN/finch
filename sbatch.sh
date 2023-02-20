577377422a3f7dc296ed5f0fda7d0572753f08ef
Tierry Hörmann <Tierry.Hoermann@meteoswiss.ch>
2023-02-20 15:51:19 +0100

    Removed legacy scripts

--- sbatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-#SBATCH --job-name="finch"
-#SBATCH --time=24:00:00
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --exclusive
-
-python run.py
--- srun.sh
+++
@@ -1,1 +0,0 @@
-srun --time=24:00:00 --exclusive --job-name="finch" --pty python run.py
